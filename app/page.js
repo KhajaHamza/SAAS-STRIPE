0efe03f5ae3aa292ec6ecdac6713d3e672d64adf
khaja hamza <khajahamza2001@gmail.com>
2024-08-17 23:52:53 -0700

    Removed some divs

--- app/page.js
+++ app/page.js
@@ -38,20 +38,6 @@ export default function Home() {
           priority
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
